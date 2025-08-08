422aef277a66986f90ff8e0f241b7177596ae0f0
jagasia <rjagadeeswaran@Yahoo.com>
2025-08-08 09:38:23 +0530

    08Aug 09:38

--- Java/java-8-features/src/AppReq1.java
+++ Java/java-8-features/src/AppReq1.java
@@ -1,12 +1,31 @@
 
+//@FunctionalInterface
+//public interface Operation {
+//	int perform(int a, int b);
+//}
+
+class JMaths{
+	public int add(int a, int b) {
+		return a+b;
+	}
+	
+	public static int difference(int a, int b) {
+		return a-b;
+	}
+}
+
 public class AppReq1 {
 
 	public static void main(String[] args) {
-		Operation op1=(x,y)->x+y;
-		Operation op2=(x,y)->x*y;
-		System.out.println(op1.perform(10, 5));
+		Operation op1=(x,y)->x+y;		//lambda, so, i wrote implementation here
+		Operation op2=(x,y)->x*y;		//lambda, so, i wrote implementation here
+//		System.out.println(op1.perform(10, 5));
+//		System.out.println(op2.perform(10, 5));
 		
-		System.out.println(op2.perform(10, 5));
+		JMaths m=new JMaths();
+		Operation op3=m::add;		//method reference
+		Operation op4=JMaths::difference;
+//		System.out.println(op3.perform(5, 6));
+		System.out.println(op4.perform(6, 3));
 	}
-
 }
