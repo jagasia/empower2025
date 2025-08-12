f3a9f95f52a04b7def2073a8118ce9f031dfef76
jagasia <rjagadeeswaran@Yahoo.com>
2025-08-12 08:22:43 +0530

    12Aug 08:22

---
+++ Java/jdbc-demo-1/src/main/java/com/empower/demo/AppSqlInjection.java
@@ -0,0 +1,34 @@
+package com.empower.demo;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import oracle.jdbc.driver.OracleDriver;
+
+public class AppSqlInjection {
+
+	public static void main(String[] args) throws SQLException {
+		Scanner sc=new Scanner(System.in);
+		String uid=sc.nextLine();
+		String pwd=sc.nextLine();
+		Driver driver = new OracleDriver();
+		String url = "jdbc:oracle:thin:@localhost:1521:XE";
+		String username = "sys as sysdba";
+		String password = "password";
+		Connection con = DriverManager.getConnection(url, username, password);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery("SELECT * FROM USER_MASTER WHERE USERNAME='"+uid+"' AND PASSWORD='"+pwd+"'");
+		if(rs.next()){
+			System.out.println("Login successful");
+		}else {
+			System.out.println("Login failed");
+		}
+		con.close();
+	}
+
+}
