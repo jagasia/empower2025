f3b704c15dc8cb6152a3f92982125e0af27d0681
jagasia <rjagadeeswaran@Yahoo.com>
2025-08-18 14:58:45 +0530

    18Aug 14:58

--- spring/maths-demo/src/main/java/controller/MathsServlet.java
+++ spring/maths-demo/src/main/java/controller/MathsServlet.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Servlet implementation class MathsServlet
@@ -35,8 +36,22 @@ public class MathsServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int no1=Integer.parseInt(request.getParameter("no1"));
 		int no2=Integer.parseInt(request.getParameter("no2"));
-	
-		response.getWriter().print(no1+no2);
+		String btn=request.getParameter("btn");
+		int result=0;
+		
+		switch(btn) {
+		case "Add":
+			result=no1+no2;
+			break;
+		case "Subtract":
+			result=no1-no2;
+			break;
+		case "Multiply":
+			result=no1*no2;
+			break;
+		}
+		PrintWriter out = response.getWriter();
+		out.print(result);
 	}
 
 }
