235d3244a03cebb69d4ba139441ef6c3613f74a8
jagasia <rjagadeeswaran@Yahoo.com>
2025-08-18 14:37:22 +0530

    18Aug 14:37

---
+++ spring/login-demo/src/main/java/controller/LoginServlet.java
@@ -0,0 +1,52 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet({ "/LoginServlet", "/login" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		String username=request.getParameter("username");
+		String password=request.getParameter("password");
+		PrintWriter out = response.getWriter();
+		if(username.equals(password)) {
+			//success
+			out.print("<p style='color:green'>Success</p>");
+		}else
+		{
+			//failure
+			out.print("<p style='color:red'>Failure</p>");
+		}
+	}
+
+}
