29359b3816b52aa1f30dfd5728fd26ca83870b92
jagasia <rjagadeeswaran@Yahoo.com>
2025-09-08 10:48:15 +0530

    08Sep 10:48

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,5 @@
-def fn1():
-    return "Hello, World!"
-
-result=fn1()
-print(result)
+#Royal Challengers Bangalore
+rcbPlayers={"Faf du Plessis","Virat Kohli","Shivam Dube","Shivam Dube","Dinesh Karthik","Anuj Rawat","Mahipal Lomror","Wanindu Hasaranga","Josh Hazlewood","Harshal Patel","Mohammed Siraj","Yuzvendra Chahal"}
+print("Welcome to Royal Challengers Bangalore")
+print("Here is the list of players in RCB")
+print(rcbPlayers)
