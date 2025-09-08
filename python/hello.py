a9d362d2ce48d92bb267cd495be160fd3ccc5b50
jagasia <rjagadeeswaran@Yahoo.com>
2025-09-08 09:54:49 +0530

    08Sep 089:54

--- python/hello.py
+++ python/hello.py
@@ -1,25 +1,5 @@
-#write a python program to print all odd numbers between 1 and 100. 
-for num in range(1, 101):
-    if num % 2 != 0:
-        print(num)  
+def fn1():
+    return "Hello, World!"
 
-    
-# the range function can also be used to generate odd numbers directly
-for num in range(1, 101, 2):
-    print(num)
-
-#apart from for loop, the range function can also be used with while loop to print odd numbers
-num = 1
-while num < 101:
-    print(num)
-    num += 2    
-
-# we can also use list comprehension to generate a list of odd numbers between 1 and 100
-odd_numbers = [num for num in range(1, 101,2)]
-print(odd_numbers)
-# we can also use filter function to filter out odd numbers from a list of numbers between 1 and 100
-numbers = list(range(1, 101))
-odd_numbers = list(filter(lambda x: x % 2 != 0, numbers))
-print(odd_numbers)
-
-
+result=fn1()
+print(result)
