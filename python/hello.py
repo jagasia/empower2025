6010b7fc370f4ba7e16f646e41fe4d322613ff92
jagasia <rjagadeeswaran@Yahoo.com>
2025-09-08 11:08:02 +0530

    08Sep 11:08

--- python/hello.py
+++ python/hello.py
@@ -1,5 +1,9 @@
 #Royal Challengers Bangalore
 rcbPlayers={"Faf du Plessis","Virat Kohli","Shivam Dube","Shivam Dube","Dinesh Karthik","Anuj Rawat","Mahipal Lomror","Wanindu Hasaranga","Josh Hazlewood","Harshal Patel","Mohammed Siraj","Yuzvendra Chahal"}
 print("Welcome to Royal Challengers Bangalore")
-print("Here is the list of players in RCB")
-print(rcbPlayers)
+try:
+    printa("Here is the list of players in RCB")
+except NameError as e:
+    print("There is an error in the code:", e)
+else:
+    print(rcbPlayers)
