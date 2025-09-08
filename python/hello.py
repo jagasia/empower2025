36c6775497b2080288b2bd9632fd7630b867337d
jagasia <rjagadeeswaran@Yahoo.com>
2025-09-08 11:47:49 +0530

    08Sep 11:47

--- python/hello.py
+++ python/hello.py
@@ -1,9 +1,9 @@
-#Royal Challengers Bangalore
-rcbPlayers={"Faf du Plessis","Virat Kohli","Shivam Dube","Shivam Dube","Dinesh Karthik","Anuj Rawat","Mahipal Lomror","Wanindu Hasaranga","Josh Hazlewood","Harshal Patel","Mohammed Siraj","Yuzvendra Chahal"}
-print("Welcome to Royal Challengers Bangalore")
-try:
-    printa("Here is the list of players in RCB")
-except NameError as e:
-    print("There is an error in the code:", e)
-else:
-    print(rcbPlayers)
+import mylib
+
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+# from mylib import fnAdd, fnSub, fnMul, fnDiv
+
+sum=mylib.fnAdd(a,b)
+
+print("Sum is: ",sum)
---
+++ python/mylib.py
@@ -0,0 +1,11 @@
+def fnAdd(a,b):
+    return a+b
+
+def fnSub(a,b):
+    return a-b
+
+def fnMul(a,b):
+    return a*b
+
+def fnDiv(a,b):
+    return a/b
