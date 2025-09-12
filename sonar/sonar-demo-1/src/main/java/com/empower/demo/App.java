3d1612921ae6e8cb3fa61fa8300374e8b54a8aea
jagasia <rjagadeeswaran@Yahoo.com>
2025-09-12 10:09:20 +0530

    12Sep 10:09

---
+++ sonar/sonar-demo-1/src/main/java/com/empower/demo/App.java
@@ -0,0 +1,18 @@
+package com.empower.demo;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Date dt=null;
+    	String name;
+    	System.out.println(dt.getHours());
+//        System.out.println( "Hello World!" );
+    }
+}
